11e3a7af8874f902b16ffbcb1661d23302f6fd82
Md. Ripon Mia <ripon@pop-os.localdomain>
2021-10-18 17:51:09 +0600

    admin panel footer issue fix

--- resources/views/backend/partials/_footer.blade.php
+++ resources/views/backend/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-  <strong>Copyright &copy; 2020-2021 <a href="http://entertechbd.com/">Entertech</a>.</strong>
+  <strong>Copyright &copy; 2020-2021 <a href="#">OCEI</a>.</strong>
   All rights reserved.
   <div class="float-right d-none d-sm-inline-block">  </div>
 </footer>
