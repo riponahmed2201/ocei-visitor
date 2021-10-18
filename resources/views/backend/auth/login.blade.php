096fb7dacc82c4ae8f6d6337eccbf83dda21a9dd
unknown <itarif.diit@gmail.com>
2021-10-18 13:22:30 +0600

    Everything is ok

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="#"><b>Login</b>Form</a>
+    <img src="{{asset('images/logo.png')}}" style="width:200px;" alt="">
   </div>
   <!-- /.login-logo -->
   <div class="card">
